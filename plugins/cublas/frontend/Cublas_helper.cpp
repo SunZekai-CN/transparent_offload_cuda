22eabccca8d04ef07b9147fda45647fc240de9ea
SunZekai-CN <sunzekai@mail.ustc.edu.cn>
2023-02-08 01:41:00 +0800

    update

--- plugins/cublas/frontend/Cublas_helper.cpp
+++ plugins/cublas/frontend/Cublas_helper.cpp
@@ -326,6 +326,30 @@ extern "C" CUBLASAPI cublasStatus_t CUBLASWINAPI cublasLtMatmulDescDestroy(){
     printf("cublasLtMatmulDescDestroy undone\n");
     assert(0);
 }
+extern "C" CUBLASAPI cublasStatus_t CUBLASWINAPI cublasCtrsmBatched( cublasHandle_t    handle,
+                                   cublasSideMode_t  side,
+                                   cublasFillMode_t  uplo,
+                                   cublasOperation_t trans,
+                                   cublasDiagType_t  diag,
+                                   int m,
+                                   int n,
+                                   const cuComplex *alpha,
+                                   const cuComplex *const A[],
+                                   int lda,
+                                   cuComplex *const B[],
+                                   int ldb,
+                                   int batchCount){
+    printf("cublasCtrsmBatched undone\n");
+    assert(0);
+                                   }
+
+
+
+
+
+
+
+
 
 
 
