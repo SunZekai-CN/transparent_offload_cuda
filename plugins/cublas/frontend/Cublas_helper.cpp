8377f62f39124e5dac96002d0fc6296cc1f7e12f
SunZekai-CN <sunzekai@mail.ustc.edu.cn>
2023-02-07 17:09:56 +0800

    update

--- plugins/cublas/frontend/Cublas_helper.cpp
+++ plugins/cublas/frontend/Cublas_helper.cpp
@@ -231,7 +231,10 @@ extern "C" CUBLASAPI cublasStatus_t CUBLASWINAPI cublasZgetrsBatched(cublasHandl
     printf("cublasZgetrsBatched undone\n");
     assert(0);
                                    }
-
+extern "C" CUBLASAPI cublasStatus_t CUBLASWINAPI cublasGetMathMode(cublasHandle_t handle, cublasMath_t *mode){
+    printf("cublasGetMathMode undone\n");
+    assert(0);
+}
 
 
 
