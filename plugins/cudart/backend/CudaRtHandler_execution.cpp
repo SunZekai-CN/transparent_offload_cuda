8f95b237d6da4aabb1eb038f48cd3571a03ea289
SunZekai-CN <sunzekai@mail.ustc.edu.cn>
2023-02-20 04:28:40 +0800

    update

--- plugins/cudart/backend/CudaRtHandler_execution.cpp
+++ plugins/cudart/backend/CudaRtHandler_execution.cpp
@@ -98,6 +98,7 @@ CUDA_ROUTINE_HANDLER(LaunchKernel) {
 
     uint64_t func = input_buffer->Get<pointer_t>();
     printf("cudaLaunchKernel - hostFunc:%x\n",func);
+    void * func = (void *) func;
     printf("aaaaaaaa\n");
     dim3 gridDim = input_buffer->Get<dim3>();
     dim3 blockDim = input_buffer->Get<dim3>();
