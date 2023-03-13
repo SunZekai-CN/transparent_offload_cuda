f5d233c0d409a60f858d210b942cfe624a415d72
SunZekai-CN <sunzekai@mail.ustc.edu.cn>
2023-03-13 11:54:08 +0800

    fix

--- plugins/cudart/util/FatBinary.cpp
+++ plugins/cudart/util/FatBinary.cpp
@@ -16,7 +16,7 @@
 #include <string.h>
 #include <map>
 
-int transfer_cronous_to_gvirtus_functions(FatBinary* fatbin_handle,map<std::string, NvInfoFunction>* mapDeviceFunc2InfoFunc){
+int transfer_cronous_to_gvirtus_functions(FatBinary* fatbin_handle,std::map<std::string, NvInfoFunction>* mapDeviceFunc2InfoFunc){
 	int count =0;
     for (auto iter = fatbin_handle->functions.begin();iter !=fatbin_handle->functions.end();++iter)
         {
